145cbaac259fbbf54464ffca73a19b969e0883dc
Iván Canuto <canutosorianoivan@gmail.com>
2023-01-22 19:12:58 +0100

    first commit

---
+++ src/data/task.js
@@ -0,0 +1,17 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "mi primera tarea",
+    description: "mi pirmera tarea",
+  },
+  {
+    id: 1,
+    title: "mi segunda tarea",
+    description: "mi segundo tarea",
+  },
+  {
+    id: 2,
+    title: "mi tercera tarea",
+    description: "mi tercera tarea",
+  },
+];
